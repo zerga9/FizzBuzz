18b4286205d202446b4d0c4d9e4a866ba5ab1465
shihhan <swang05@mail.bbk.ac.uk>
2018-03-13 13:31:10 +0000

    change testing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -15,4 +15,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 15' do
     expect(fizzbuzz(19)).to eq 19
   end
+  it 'returns "fizz" when passed 15' do
+    expect(fizzbuzz(97)).to eq 97
+  end
 end
